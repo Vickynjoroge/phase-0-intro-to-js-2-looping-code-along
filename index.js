08936f4ac45520eecf580079e7e746a58d4d9755
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-03-11 22:20:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+let event = ["Surprise"];
+
+function writeCards(name, events) {
+    const message = [];
+    for (let i = 0; i < name.length; i++) {
+            message.push(`Thank you, ${name[i]}, for the wonderful ${events} gift!`);
+    }
+    return message;
+}
+
+function countDown (n){
+    let countDown = n;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+countDown(10);
